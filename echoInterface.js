5aabe8b620ae4f7bda053a485d9b9302936124a8
dontroll17 <dontroll@yandex.ru>
2022-02-01 17:08:44 +0300

    new echo interface

---
+++ echoInterface.js
@@ -0,0 +1,18 @@
+const { createInterface } = require('readline');
+const { appendFile } = require('fs/promises');
+//create new interface
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+//listen to an event
+rl.on('line', line => {
+    //cache the line
+	let copy = line;
+    //write line in file
+    appendFile('stdout.txt', `${copy}\n`);
+});
+
+rl.on('close', () => {
+	console.log('Close interface');
+});
